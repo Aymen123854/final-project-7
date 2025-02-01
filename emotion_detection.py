543224640646c62c00ce7c5179481b6e2ac88e1f
“Aymen123854” <aymenwaqas24@gmail.com>
2025-02-01 08:03:53 -0500

    New changes 02/02/2025

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,1 +1,24 @@
 # Addition of new file emotion_detection.py
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json().get('text')
+
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred: {e}"
+    
+print(emotion_detector("I love this new technology."))
